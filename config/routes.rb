046e2ed5abbeaf1f6eabcb21ed747f19da876202
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2024-01-24 19:18:54 +0000

    test phase

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update, :destroy]
   resources :books, only: [:index, :show, :edit, :update, :create, :destroy]
 
-  get 'homes/about', to:'homes#about',as: 'about'
+  get 'home/about', to:'homes#about',as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
